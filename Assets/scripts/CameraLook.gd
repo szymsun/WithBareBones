a9cb04c36589b3afb5f22eb262cdf5e3ce0878fe
szymsun <szy.gladysz@proton.me>
2024-08-19 14:45:41 +0200

    WIP

--- Assets/scripts/CameraLook.gd
+++ Assets/scripts/CameraLook.gd
@@ -1,11 +1,36 @@
 extends Node3D
 
+var pause_bool := false
+
+@export var mouseSensitivity := 0.01
+var MouseX := 0.0
+var MouseY := 0.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) # Replace with function body.
 
+
+func _on_pause_signal():
+	if pause:
+		resume()
+	else:
+		pause()
+	
+
+func pause():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	pause_bool = true
+	
+func resume():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pause_bool = false
+
+
+func _unhandled_input(event):
+	pass	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("ui_cancel"):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
